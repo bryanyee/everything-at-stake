c7d99ff8c1bacd075f0e89621295ee83ce456e98
Bryan Yee <yee.bryan.c@gmail.com>
2021-09-19 10:46:28 -0700

    Add to cardano node animation

--- components/CardanoNode.js
+++ components/CardanoNode.js
@@ -1,5 +1,35 @@
+import { useState, useEffect, useRef } from 'react';
+import faker from 'faker';
+import shajs from 'sha.js';
 import styles from '../styles/CardanoNode.module.css';
 
+function getRandomString() {
+  return faker.lorem.words();
+}
+
+function getSha(string) {
+  return shajs('sha256').update(string).digest('hex');
+}
+
 export default function CardanoNode() {
-  return <div className={styles['cardano-node']} />;
+  const [string, setString] = useState('');
+  const [result, setResult] = useState('');
+  let intervalRef = useRef(null);
+
+  useEffect(() => {
+    intervalRef.current = setInterval(() => {
+      setString(getRandomString());
+    }, 2000);
+
+    return () => {
+      clearInterval(intervalRef.current);
+    }
+  }, []);
+
+  return (
+    <div className={styles['node-wrapper']}>
+      <div className={styles['cardano-node']} />
+      <div className={styles['string-input']}>{string}</div>
+    </div>
+  );
 }
