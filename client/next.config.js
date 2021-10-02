907c688f3e5f1f3baa329630366e6bafb65ef8ff
Bryan Yee <yee.bryan.c@gmail.com>
2021-10-01 20:59:49 -0700

    Fix scroll restoration on back/forward navigation

--- client/next.config.js
+++ client/next.config.js
@@ -1,3 +1,9 @@
 module.exports = {
+  // Awaiting officiak fix for scroll restoration on back/forward navigation
+  // https://github.com/vercel/next.js/issues/20951
+  // https://mmazzarolo.com/blog/2021-04-10-nextjs-scroll-restoration/
+  experimental: {
+    scrollRestoration: true,
+  },
   reactStrictMode: true,
 }
